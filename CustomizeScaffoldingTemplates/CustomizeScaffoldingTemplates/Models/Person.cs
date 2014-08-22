a4508f5fac6a4bb316fa9794bba0edb8467db10a
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2014-08-22 15:51:48 -0700

    I modified the templates to generate modified content, see how here

---
+++ CustomizeScaffoldingTemplates/CustomizeScaffoldingTemplates/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CustomizeScaffoldingTemplates
+{
+    /// <summary>
+    public class Person
+    {
+        public int PersonId { get; set; }
+
+        public string Name { get; set; }
+
+        public bool BoolProperty { get; set; }
+    }
+}
